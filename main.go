7dbcef42a97f7ba6279432f36e25eb91ab595f23
Stanley Yuen <102275989+stanleyyconsensys@users.noreply.github.com>
2022-10-14 16:27:41 +0800

    update

--- main.go
+++ main.go
@@ -14,7 +14,93 @@ type Keys struct {
 	publicKeys [2][256][]byte
 }
 
-func Hash(h hash.Hash, r []byte) []byte {
+
+type Lamport struct {
+	hashFunc  func() hash.Hash
+}
+
+func pickBit(x *big.Int, shiftOffset int) uint64 {
+	x.Rsh(x, uint(shiftOffset))
+	return new(big.Int).And( x, big.NewInt(1) ).Uint64()
+}
+
+func NewLamport(h func() hash.Hash) *Lamport {
+	return &Lamport{
+		hashFunc: h,
+	}
+}
+
+func (l *Lamport) GenerateKey() ([2][256][]byte, [2][256][]byte, error)  {
+
+	var privateKeys [2][256][]byte
+	var publicKeys [2][256][]byte
+
+	h := l.hashFunc()
+
+	for  i := 0; i < 256; i++ {
+
+		key1,_ := RandomByte(32)
+		key2,_ := RandomByte(32)
+
+		privateKeys[0][i] = key1
+		privateKeys[1][i] = key2
+		publicKeys[0][i] = Hash(h, key1)
+		publicKeys[1][i] = Hash(h, key2)
+	}
+	return privateKeys, publicKeys, nil
+}
+
+func (l *Lamport) Sign(msg []byte, sk [2][256][]byte) [][]byte {
+	var sig [][]byte
+	h := l.hashFunc()
+	//hashes the message to a 256-bit hash
+	encoded := Hash(h, msg)
+
+	//convert byte[] to big int
+	x := new(big.Int).SetBytes(encoded)
+	
+	var b uint64
+
+	for  i := 0; i < 256; i++ {
+		// same operation as  int << i & 1 
+		if i == 0 {
+			b = pickBit(x, 0)
+		} else {
+			b = pickBit(x, 1)
+		}
+		
+		// b = 1 or 0
+		sig = append(sig, sk[b][i])
+	}
+	return sig
+}
+
+func (l *Lamport) Verify(msg []byte, sig [][]byte,  pk [2][256][]byte) bool {
+	h := l.hashFunc()
+	encoded := Hash(h, msg)
+
+	x := new(big.Int).SetBytes(encoded)
+
+	var b uint64
+
+	for  i := 0; i < 256; i++ {
+		
+		if i == 0 {
+			b = pickBit(x, 0)
+		} else {
+			b = pickBit(x, 1)
+		}
+
+		hashSign := Hash(h, sig[i])
+		
+		if !bytes.Equal(pk[b][i], hashSign) {
+			return false
+		}
+	}
+	return true
+}
+
+func Hash(h hash.Hash , r []byte) []byte {
 	h.Reset()
 	h.Write(r)
 	return h.Sum(nil)
@@ -28,73 +114,16 @@ func RandomByte(size int) ([]byte, error) {
 	return key, nil
 }
 
-func GenerateKey() (*Keys, error)  {
 
-	var privateKeys [2][256][]byte
-	var publicKeys [2][256][]byte
-	h := sha256.New()
-
-	for  i := 0; i < 256; i++ {
-
-		key1,_ := RandomByte(32)
-		key2,_ := RandomByte(32)
-
-		privateKeys[0][i] = key1
-		privateKeys[1][i] = key2
-		publicKeys[0][i] = Hash(h, key1)
-		publicKeys[1][i] = Hash(h, key2)
-	}
-	return &Keys{privateKeys: privateKeys, publicKeys: publicKeys}, nil
-}
-
-
-
-func Sign(msg []byte, sk [2][256][]byte) [][]byte {
-	var sig [][]byte
-
-	h := sha256.New()
-	//hashes the message to a 256-bit hash
-	encoded := Hash(h, msg)
-
-	//convert byte[] to big int
-	var bI big.Int
-	x := bI.SetBytes(encoded)
-
-	//fmt.Printf( "%b\n",x )
-	//fmt.Printf( "%b\n",data )
-
-	for  i := 0; i < 256; i++ {
-		// same operation as  int << i & 1 
-
-		b := new(big.Int).And( new(big.Int).Rsh(x, uint(i)), big.NewInt(1) ).Uint64()
-
-		// b = 1 or 0
-		sig = append(sig, sk[b][i])
-	}
-	return sig
-}
-
-
-func Verify(msg []byte, sig [][]byte,  pk [2][256][]byte) bool {
-	h := sha256.New()
-	encoded := Hash(h, msg)
-
-	var bI big.Int
-	x := bI.SetBytes(encoded)
-
-	for  i := 0; i < 256; i++ {
-		b := new(big.Int).And( new(big.Int).Rsh(x, uint(i)), big.NewInt(1) ).Uint64()
-		hashSign := Hash(h, sig[i])
-		if !bytes.Equal(pk[b][i], hashSign) {
-			return false
-		}
-	}
-	return true
-}
 
 func main() {
-	keys, _ := GenerateKey()
 	msg := []byte("陳生")
-	sig := Sign(msg, keys.privateKeys)
-	fmt.Println( Verify(msg, sig, keys.publicKeys) )
+
+	lamport := NewLamport(sha256.New)
+
+	sk, pk, _ := lamport.GenerateKey()
+
+	sig := lamport.Sign(msg, sk)
+	
+	fmt.Println( lamport.Verify(msg, sig, pk) )
 }
